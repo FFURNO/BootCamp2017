e439f39d308205fb073ab3874660d69932b3fd62
Francesco Furno <furno.francesco@gmail.com>
2017-06-26 19:12:44 -0500

    Problem Set 1 Computation

---
+++ ProblemSets/Comp/calculator.py
@@ -0,0 +1,16 @@
+''' This is my calculator.py function which
+I use for problem 3 of the Standard Library Problem Set '''
+
+def sum(x,y):
+    result = x + y
+    return result
+
+
+def product(x,y):
+    result = x * y
+    return result
+
+
+def squareroot(x):
+    result = x**(1/2)
+    return result
